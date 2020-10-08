7f92bfcb92c090319c8d99c210a2f331ffee8e8f
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-10-08 15:53:54 -0500

    all animals are on a feeding schedule

--- animals_handler.py
+++ animals_handler.py
@@ -1,22 +1,44 @@
-from datetime import date
 from petting import Llama, Goat, Alpaca, Pony, Pig
 from pond import Tadpole, Goldfish, Mallard, Goose, Frog
 from tank import Snake, Turtle, Lizard, Viper, Boa
 
-miss_fuzz = Llama("Miss Fuzz", "domestic llama")
-mr_fuzz = Goat("Mister Fuzz", "mountain goat")
-baby_fuzz = Alpaca("Baby Fuzz", "domestic alpaca")
-fancy_fuzz = Pony("Fancy Fuzz", "miniature horse")
-oink_fuzz = Pig("Oink Fuzz", "domestic pig")
+miss_fuzz = Llama("Miss Fuzz", "domestic llama", "morning", "Llama Chow" )
+# print(miss_fuzz.feed())
+mr_fuzz = Goat("Mister Fuzz", "mountain goat", "morning", "Goat Chow")
+# print(mr_fuzz.feed())
+baby_fuzz = Alpaca("Baby Fuzz", "domestic alpaca", "morning", "Alpaca Chow")
+# print(baby_fuzz.feed())
+fancy_fuzz = Pony("Fancy Fuzz", "miniature horse", "morning", "Pony Chow")
+# print(fancy_fuzz.feed())
+oink_fuzz = Pig("Oink Fuzz", "domestic pig", "morning", "Pig Chow")
+#print(oink_fuzz.feed())
+
+
 ##########
-sneky = Snake("Sneky", "copperhead")
-squirt = Turtle("Squirt", "turtle")
-lizzie = Lizard("Lizzie", "lizard")
-veep = Viper("Veep", "viper")
-boaz = Boa("Boaz", "boa constrictor")
+
+
+sneky = Snake("Sneky", "copperhead", "morning", "Snake Chow")
+print(sneky.feed())
+squirt = Turtle("Squirt", "turtle", "morning", "Turtle Chow")
+print(squirt.feed())
+lizzie = Lizard("Lizzie", "lizard", "morning", "Lizard Chow")
+print(lizzie.feed())
+veep = Viper("Veep", "viper", "morning", "Pig Chow")
+print(veep.feed())
+boaz = Boa("Boaz", "boa constrictor", "morning", "Boa Chow")
+print(boaz.feed())
+
+
 ###############
-thad = Tadpole("Thad", "frog")
-nemo = Goldfish("Nemo", "fish")
-mallory = Mallard("Mallory", "duck")
-bonkers = Goose("Bonkers", "goose")
-froggie = Frog("Froggie", "Frog")
+
+
+thad = Tadpole("Thad", "frog", "morning", "Snake Chow")
+# print(thad.feed())
+nemo = Goldfish("Nemo", "fish", "morning", "Snake Chow")
+# print(nemo.feed())
+mallory = Mallard("Mallory", "duck", "morning", "Snake Chow")
+# print(mallory.feed())
+bonkers = Goose("Bonkers", "goose", "morning", "Snake Chow")
+# print(bonkers.feed())
+froggie = Frog("Froggie", "Frog", "morning", "Snake Chow")
+# print(froggie.feed())
--- tank/animals.py
+++ tank/animals.py
@@ -3,40 +3,60 @@ from datetime import date
 
 class Snake: 
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
+        self.food = food
+
+    def feed(self):
+      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 class Turtle: 
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.food = food
+
+    def feed(self):
+      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 class Lizard: 
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
+        self.food = food
+
+    def feed(self):
+      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 class Viper: 
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
+        self.food = food
+
+    def feed(self):
+      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 class Boa: 
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
+        self.food = food
+
+    def feed(self):
+      print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
